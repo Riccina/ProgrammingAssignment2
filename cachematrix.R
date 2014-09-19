f62a63995b89c495fa666475d132ed745eb6a8df
Riccina <msdaelia@gmail.com>
2014-09-19 21:12:27 +1000

    Writing two functions to cache the inverse of a matrix.
    Main script

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Writing a function that can cache the inverse of a matrix
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(v = matrix()) {
+                
+                ## Defining the inverse property
+                Inv <- NULL
+                
+                ## 1. set the value of the matrix
+                set <- function(matrix) {
+                        v <<- matrix
+                        Inv <<- NULL
+                }
+                
+                ## 2. get the value of the matrix
+                get <- function() v
+                
+                ## 3. set the value of inverse of the matrix
+                setInverse <- function(Inverse) Inv <<- Inverse
+                
+                ## 4. get the value of inverse of the matrix
+                getInverse<- function() Inv
+                list(set = set, get = get,
+                     setInverse = setInverse,
+                     getmean = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above.
+## This function assumes that the matrix is always invertible.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        v <- x$getinverse()
+        if(!is.null(v)) {
+                message("getting cached data.")
+                return(v)
+        }
+        data <- x$get()
+        
+        ##Calculating the inverse
+        v <- solve(data)
+        
+        ##Setting the inverse to the object
+        x$setinverse(v)
+        
+        ##Returning the inverted matrix
+        v  
+        
 }
